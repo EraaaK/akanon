b4ed9e0ff9dbd37a3b80093c86aa51f5cd49eb01
William <williamfwddg@gmail.com>
2022-07-17 15:01:14 -0300

    Ticket exemplo retornando, configurações html e python iniciais PRONTAS. Agora iniciar o link das apis para o front.

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+import sqlite3
 from flask import Flask, render_template
-from api import InboxAPI
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    conn = get_db_connection()
+    tickets = conn.execute('SELECT * from tickets').fetchall()
+    conn.close()
+    return render_template('index.html', tickets=tickets)
 
 
 app.run()
