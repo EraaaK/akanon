6478b34a89dc5384488c0749fad100974f38c021
William <williamfwddg@gmail.com>
2022-07-17 14:37:20 -0300

    Criação do schema para armazenar os tickets

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS tickets;
+
+CREATE TABLE tickets
+(
+    ticket_number TEXT,
+    ticket_status TEXT,
+    ticket_agent TEXT,
+    ticket_type TEXT,
+    ticket_group TEXT,
+    ticket_sla TEXT,
+    ticket_url TEXT
+);
